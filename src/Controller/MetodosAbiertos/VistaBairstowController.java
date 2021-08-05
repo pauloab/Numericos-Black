aa24c091c705e5a2b99a8c25de46418d4201db85
pauloab <pauloaguilar2001@gmail.com>
2021-08-05 10:46:47 -0500

    Remoción de componentes para ingreso de formulas

--- src/Controller/MetodosAbiertos/VistaBairstowController.java
+++ src/Controller/MetodosAbiertos/VistaBairstowController.java
@@ -3,25 +3,18 @@ package Controller.MetodosAbiertos;
 import Modelos.Excepciones.IteracionesAlcanzadas;
 import Modelos.Excepciones.ValoresResueltos;
 import Modelos.MetodosAbiertos.Bairstow;
-import Vistas.Components.Math.FormulaFunctionButtons;
-import Vistas.Components.Math.FormulaInput;
 import Util.Graficos;
-import Util.MetodosUniversales;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 /**
--- src/Vistas/Components/Math/Deleteable.java
+++
@@ -1,9 +0,0 @@
-package Vistas.Components.Math;
-
-/**
- * Interfaz para elementos eliminables
- * @author Paulo Aguilar
- */
-public interface Deleteable {
-    public void deleteMe(GenericComponent caller);
-}
