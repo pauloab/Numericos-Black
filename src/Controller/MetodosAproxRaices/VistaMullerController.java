cc9e9945d33830e8d7e10b8e6a1714edbbe24569
pauloab <pauloaguilar2001@gmail.com>
2021-08-07 21:11:38 -0500

    Implementación de gráfica - Muller

--- src/Controller/MetodosAproxRaices/VistaMullerController.java
+++ src/Controller/MetodosAproxRaices/VistaMullerController.java
@@ -1,15 +1,20 @@
 package Controller.MetodosAproxRaices;
 
 import Modelos.MetodosAproxRaices.Muller;
+import Plotter.Models.CoordinatePair;
+import Plotter.Views.GraphManager;
 import Util.Graficos;
 import Util.Matematico;
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 
@@ -40,12 +45,43 @@ public class VistaMullerController implements Initializable {
     private Button btProcesar;
     @FXML
     private Button btLimpiar;
+    @FXML
+    private BorderPane bpChart;
+
+    @FXML
+    private TextField tfYU, tfYD, tfXL, tfXR;
+    @FXML
+    private JFXButton btAjustar;
+    private GraphManager graphManager;
+    private double yu = 50, yd = -50, xl = -50, xr = 50;
+    private final double DEFAULT_AXIS_VALUES = 50;
+    private String funcion;
+    private Double punto = null;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        graphManager = new GraphManager();
+        tfXL.setText(xl + "");
+        tfXR.setText(xr + "");
+        tfYU.setText(yu + "");
+        tfYD.setText(yd + "");
+        definirLimites();
+
+        bpChart.setCenter(graphManager.getGraph());
+        funcion = null;
+        btAjustar.setOnMouseClicked(event -> {
+            boolean limitesDefinidos = definirLimites();
+            if (funcion != null && limitesDefinidos) {
+                Graficar();
+            }
+        });
+
+        Graficos.convertirEnInputFlotantes(tfXL);
+        Graficos.convertirEnInputFlotantes(tfXR);
+        Graficos.convertirEnInputFlotantes(tfYU);
+        Graficos.convertirEnInputFlotantes(tfYD);
         
-        
-        // Se agrega la validación de los inputs
         Graficos.convertirEnInputFlotantes(tfx0);
         Graficos.convertirEnInputFlotantes(tfx1);
         Graficos.convertirEnInputFlotantes(tfx2);
@@ -53,7 +89,7 @@ public class VistaMullerController implements Initializable {
         Graficos.convertirEnInputEnteros(tfIterMax);
         
         btProcesar.setOnMouseClicked(event -> {
-            String funcion = tfFormula.getText();
+            funcion = tfFormula.getText();
             Muller muller;
             boolean error = false;
             if (Matematico.validarExpresion(funcion)) {
@@ -63,9 +99,9 @@ public class VistaMullerController implements Initializable {
                 Double eTolerancia = Graficos.validarTextFieldDouble(tfErrorTolerancia);
                 Integer imax = Graficos.validarTextFieldEnteros(tfIterMax);
                 if (x0 != null && x1 != null && x2 != null && eTolerancia != null && imax != null) {
-                    muller = new Muller(funcion, eTolerancia, imax, x0, x1, x2);
+                    muller = new Muller(funcion, eTolerancia, imax, x2, x1, x0);
                     try {
-                        muller.metodoMuller();
+                        punto = muller.metodoMuller();
                         String[] headers = {"x0", "x1", "x2", "xr", "Error de aproximación"};
                         Graficos.cargarEnTableView(tvResultados, muller.getMatrizDeDatos(), headers);
                     } catch (Exception ex) {
@@ -75,14 +111,7 @@ public class VistaMullerController implements Initializable {
                                 + "a travéz de este método.");
                     }
                     if (!error) {
-                        try {
-                           // Aquí se debe cargar la gráfica
-                        } catch (Exception e) {
-                            Graficos.lanzarMensajeError("Error de Graficación", "Tuvimos un inconveniente al "
-                                    + "interpretar o procesar la función "
-                                    + "a travéz de este método, por tanto"
-                                    + "la gráfica no se pudo procesar.");
-                        }
+                        Graficar();
                     }    
                 }else{
                     Graficos.lanzarMensajeError("Error de conversión","Por favor, verifica el ingreso de datos antes de proceder.");
@@ -92,6 +121,15 @@ public class VistaMullerController implements Initializable {
             }
         });
         btLimpiar.setOnMouseClicked(e -> {
+            graphManager.getGraph().getData().clear();
+            funcion = null;
+            graphManager.setDomain(-DEFAULT_AXIS_VALUES, DEFAULT_AXIS_VALUES);
+            graphManager.setRange(-DEFAULT_AXIS_VALUES, DEFAULT_AXIS_VALUES);
+            tfXL.setText("-" + DEFAULT_AXIS_VALUES);
+            tfXR.setText("" + DEFAULT_AXIS_VALUES);
+            tfYU.setText("" + DEFAULT_AXIS_VALUES);
+            tfYD.setText("-" + DEFAULT_AXIS_VALUES);
+            definirLimites();
             tfFormula.clear();
             tfErrorTolerancia.setText("");
             tfIterMax.setText("");
@@ -101,4 +139,48 @@ public class VistaMullerController implements Initializable {
             tvResultados.getItems().clear();
         });
     }    
+    
+     private void Graficar() {
+        try {
+            CoordinatePair puntoCords = new CoordinatePair(punto,
+                    Matematico.evaluarFuncion(funcion, punto));
+
+            ArrayList<CoordinatePair[]> dataset = new ArrayList<>();
+            dataset.add(Matematico.evaluarFuncion(funcion, xl, xr));
+            Graficos.plotPoints(dataset, bpChart, graphManager, puntoCords);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Graficos.lanzarMensajeError("Error de Graficación", "Tuvimos un inconveniente al "
+                    + "interpretar o procesar la función "
+                    + "a travéz de este método, por tanto"
+                    + "la gráfica no se pudo procesar.");
+        }
+    }
+
+    private boolean definirLimites() {
+        boolean res = true;
+        Double xl = Graficos.validarTextFieldDouble(tfXL);
+        Double xr = Graficos.validarTextFieldDouble(tfXR);
+        Double yu = Graficos.validarTextFieldDouble(tfYU);
+        Double yd = Graficos.validarTextFieldDouble(tfYD);
+        if (xl != null && xr != null && yu != null && yd != null) {
+            if (xl < xr && yu > yd) {
+                this.xl = xl;
+                this.xr = xr;
+                this.yu = yu;
+                this.yd = yd;
+                graphManager.setDomain(xl, xr);
+                graphManager.setRange(yd, yu);
+            } else {
+                Graficos.lanzarMensajeAdvertencia("Verifique los intervalos.",
+                        "Verifique que el rango y el dominio. El intervalo debe ir de menor a mayor.");
+                res = false;
+            }
+        } else {
+            Graficos.lanzarMensajeError("Error de conversión.",
+                    "Verifique los valores ingresados en los campos de control de gráfica.");
+            res = false;
+        }
+        return res;
+    }
 }
