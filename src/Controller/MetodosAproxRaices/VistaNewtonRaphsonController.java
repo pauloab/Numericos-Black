8e2e078c5106c2cd0e04f4776f39dbdc667869e0
jmatamoro5 <jmatamoro5@utmachala.edu.ec>
2021-08-09 21:07:49 -0500

    Implementacion del grafico en Newton Raphson

--- src/Controller/MetodosAproxRaices/VistaNewtonRaphsonController.java
+++ src/Controller/MetodosAproxRaices/VistaNewtonRaphsonController.java
@@ -1,14 +1,19 @@
 package Controller.MetodosAproxRaices;
 import Modelos.MetodosAproxRaices.NewtonRaphson;
+import Plotter.Models.CoordinatePair;
+import Plotter.Views.GraphManager;
 import Util.Graficos;
 import Util.Matematico;
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
 
@@ -35,11 +40,42 @@ public class VistaNewtonRaphsonController implements Initializable {
     private Button btProcesar;
     @FXML
     private Button btLimpiar;
+    @FXML
+    private BorderPane bpChart;
+    @FXML
+    private TextField tfYU, tfYD, tfXL, tfXR;
+    @FXML
+    private JFXButton btAjustar;
+    private GraphManager graphManager;
+    private double yu = 50, yd = -50, xl = -50, xr = 50;
+    private final double DEFAULT_AXIS_VALUES = 50;
+    private String funcion;
+    private Double punto = null;
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        graphManager = new GraphManager();
+        tfXL.setText(xl + "");
+        tfXR.setText(xr + "");
+        tfYU.setText(yu + "");
+        tfYD.setText(yd + "");
+        definirLimites();
+
+        bpChart.setCenter(graphManager.getGraph());
+        funcion = null;
+        btAjustar.setOnMouseClicked(event -> {
+            boolean limitesDefinidos = definirLimites();
+            if (funcion != null && limitesDefinidos) {
+                Graficar();
+            }
+        });
+
+        Graficos.convertirEnInputFlotantes(tfXL);
+        Graficos.convertirEnInputFlotantes(tfXR);
+        Graficos.convertirEnInputFlotantes(tfYU);
+        Graficos.convertirEnInputFlotantes(tfYD);
         
         // Se agrega la validación de los inputs
         Graficos.convertirEnInputFlotantes(tfx0);
@@ -47,7 +83,7 @@ public class VistaNewtonRaphsonController implements Initializable {
         Graficos.convertirEnInputEnteros(tfIterMax);
         
         btProcesar.setOnMouseClicked(event -> {
-            String funcion = tfFormula.getText();
+            funcion = tfFormula.getText();
             NewtonRaphson newtonraphson;
             boolean error = false;
             if (Matematico.validarExpresion(funcion)) {
@@ -57,7 +93,7 @@ public class VistaNewtonRaphsonController implements Initializable {
                 if (x0 != null && eTolerancia != null && imax != null) {
                     newtonraphson = new NewtonRaphson(funcion, eTolerancia, imax, x0);
                     try {
-                        newtonraphson.metodoNewtonRaphson();
+                        punto = newtonraphson.metodoNewtonRaphson();
                         String[] headers = {"xi+1","f(xi)", "f'(xi)", "Error de aproximación"};
                         Graficos.cargarEnTableView(tvResultados, newtonraphson.getMatrizDeDatos(), headers);
                     } catch (Exception ex) {
@@ -67,14 +103,7 @@ public class VistaNewtonRaphsonController implements Initializable {
                                 + "a travéz de este método.");
                     }
                     if (!error) {
-                        try {
-                           // Aquí se debe cargar la gráfica
-                        } catch (Exception e) {
-                            Graficos.lanzarMensajeError("Error de Graficación", "Tuvimos un inconveniente al "
-                                    + "interpretar o procesar la función "
-                                    + "a travéz de este método, por tanto"
-                                    + "la gráfica no se pudo procesar.");
-                        }
+                        Graficar();
                     }    
                 }else{
                     Graficos.lanzarMensajeError("Error de conversión","Por favor, verifica el ingreso de datos antes de proceder.");
@@ -90,5 +119,48 @@ public class VistaNewtonRaphsonController implements Initializable {
             tfx0.setText("");
             tvResultados.getItems().clear();
         });
-    }    
+    } 
+     private void Graficar() {
+        try {
+            CoordinatePair puntoCords = new CoordinatePair(punto,
+                    Matematico.evaluarFuncion(funcion, punto));
+
+            ArrayList<CoordinatePair[]> dataset = new ArrayList<>();
+            dataset.add(Matematico.evaluarFuncion(funcion, xl, xr));
+            Graficos.plotPoints(dataset, bpChart, graphManager, puntoCords);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Graficos.lanzarMensajeError("Error de Graficación", "Tuvimos un inconveniente al "
+                    + "interpretar o procesar la función "
+                    + "a travéz de este método, por tanto"
+                    + "la gráfica no se pudo procesar.");
+        }
+    }
+
+    private boolean definirLimites() {
+        boolean res = true;
+        Double xl = Graficos.validarTextFieldDouble(tfXL);
+        Double xr = Graficos.validarTextFieldDouble(tfXR);
+        Double yu = Graficos.validarTextFieldDouble(tfYU);
+        Double yd = Graficos.validarTextFieldDouble(tfYD);
+        if (xl != null && xr != null && yu != null && yd != null) {
+            if (xl < xr && yu > yd) {
+                this.xl = xl;
+                this.xr = xr;
+                this.yu = yu;
+                this.yd = yd;
+                graphManager.setDomain(xl, xr);
+                graphManager.setRange(yd, yu);
+            } else {
+                Graficos.lanzarMensajeAdvertencia("Verifique los intervalos.",
+                        "Verifique que el rango y el dominio. El intervalo debe ir de menor a mayor.");
+                res = false;
+            }
+        } else {
+            Graficos.lanzarMensajeError("Error de conversión.",
+                    "Verifique los valores ingresados en los campos de control de gráfica.");
+            res = false;
+        }
+        return res;
+    }
 }
